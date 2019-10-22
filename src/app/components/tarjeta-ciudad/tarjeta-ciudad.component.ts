af72f9be6782deb5010f80ee3f3dbadc65b9105c
Gero <geronimo@devsar.com>
2019-10-22 10:42:51 -0300

    First version, original ionic card

--- src/app/components/tarjeta-ciudad/tarjeta-ciudad.component.ts
+++ src/app/components/tarjeta-ciudad/tarjeta-ciudad.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tarjeta-ciudad',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tarjeta-ciudad.component.scss'],
 })
 export class TarjetaCiudadComponent implements OnInit {
+
+  @Input() citydata;
 
   constructor() { }
 
