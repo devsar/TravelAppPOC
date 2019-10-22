1fa8f8c62c3ca997aa182f4c880615f97bd9c82e
Gero <geronimo@devsar.com>
2019-10-22 11:12:19 -0300

    added cities to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { CiudadPage } from './modules/ciudad/ciudad.page';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./modules/home/home.module').then( m => m.HomePageModule)},
+  { path: 'city/:name',  loadChildren: () => import('./modules/ciudad/ciudad.module').then( m => m.CiudadModule)},
 ];
 
 @NgModule({
