eb6d2fb1e7e6130b3eb57a8b900c73dbbdd7f02a
Gero <geronimo@devsar.com>
2019-10-22 11:13:04 -0300

    Added city page

--- src/app/modules/ciudad/ciudad.module.ts
+++ src/app/modules/ciudad/ciudad.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { CiudadPage } from './ciudad.page';
+import { CityHeaderComponent } from 'src/app/components/city-header/city-header.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CiudadPage, CityHeaderComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: CiudadPage
+      }
+    ])
+  ],
 })
 export class CiudadModule { }
