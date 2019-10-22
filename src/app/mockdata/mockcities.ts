71319b1154295d0e3363dfeec0a6da22c9cafad6
Gero <geronimo@devsar.com>
2019-10-22 10:43:19 -0300

    Added city state

--- src/app/mockdata/mockcities.ts
+++ src/app/mockdata/mockcities.ts
@@ -1,29 +1,34 @@
 import { Ciudad } from '../types/ciudad';
 
 export const CIUDADES: Ciudad[] = [
-    {id: 1, name: 'São Paulo', img: 'https://cdn.getyourguide.com/img/tour_img-2043232-145.jpg', description: 'Hermosa ciudad'},
     // tslint:disable-next-line: max-line-length
-    {id: 2, name: 'Rio de Janeiro', img: 'https://www.avianca.com/content/dam/avianca_new/destinos/semana/gig/1-porque-visitarla/destino-rio-de-janeiro-brazil-para-conocer-el-cerro-del-corcovado.jpg', description: 'Hermosa ciudad'},
-    {id: 3, name: 'Gramado', img: 'https://surenio.com.ar/wp-content/uploads/2017/12/gramado-7.jpg', description: 'Hermosa ciudad'},
+    {id: 1, name: 'São Paulo', state: 'São Paulo', img: 'https://cdn.getyourguide.com/img/tour_img-2043232-145.jpg', description: 'Hermosa ciudad'},
     // tslint:disable-next-line: max-line-length
-    {id: 4, name: 'Salvador', img: 'https://static7lonelyplanetes.cdnstatics.com/sites/default/files/styles/max_1300x1300/public/blog/brasil_salvadorbahia_09-bestintrvavel2018_gettyimages-488430980_jeilson_istockphoto_getty_images.jpg?itok=W_X67q2T', description: 'Hermosa ciudad'},
+    {id: 2, name: 'Rio de Janeiro', state: 'Rio de Janeiro', img: 'https://www.avianca.com/content/dam/avianca_new/destinos/semana/gig/1-porque-visitarla/destino-rio-de-janeiro-brazil-para-conocer-el-cerro-del-corcovado.jpg', description: 'Hermosa ciudad'},
     // tslint:disable-next-line: max-line-length
-    {id: 5, name: 'Florianópolis', img: 'http://zanoliviajes.com.ar/wp-content/uploads/2017/10/Floria-3.jpg', description: 'Hermosa ciudad'},
+    {id: 3, name: 'Gramado', state: 'Río Grande do Sul', img: 'https://surenio.com.ar/wp-content/uploads/2017/12/gramado-7.jpg', description: 'Hermosa ciudad'},
     // tslint:disable-next-line: max-line-length
-    {id: 6, name: 'Curitiba', img: 'https://i.pinimg.com/originals/9c/5c/23/9c5c23a403abf0226276d4724396fa18.jpg', description: 'Hermosa ciudad'},
+    {id: 4, name: 'Salvador', state: 'Bahía', img: 'https://static7lonelyplanetes.cdnstatics.com/sites/default/files/styles/max_1300x1300/public/blog/brasil_salvadorbahia_09-bestintrvavel2018_gettyimages-488430980_jeilson_istockphoto_getty_images.jpg?itok=W_X67q2T', description: 'Hermosa ciudad'},
     // tslint:disable-next-line: max-line-length
-    {id: 7, name: 'Fortaleza', img: 'https://www.turismobr.com/wp-content/uploads/2011/01/Fortaleza-desde-sus-playas-760x500.jpg', description: 'Hermosa ciudad'},
+    {id: 5, name: 'Florianópolis', state: 'Santa Catarina', img: 'http://zanoliviajes.com.ar/wp-content/uploads/2017/10/Floria-3.jpg', description: 'Hermosa ciudad'},
     // tslint:disable-next-line: max-line-length
-    {id: 8, name: 'Natal', img: 'https://d1cfr5yzsdeyhu.cloudfront.net/blog/wp-content/uploads/2016/04/12162253/Tips-para-viajar-a-Natal.jpg', description: 'Hermosa ciudad'},
-    {id: 9, name: 'Trancoso', img: 'https://dla.cdncimeco.com/uploads/2018/12/image5c27e80068e06.jpg', description: 'Hermosa ciudad'},
+    {id: 6, name: 'Curitiba', state: 'Paraná', img: 'https://i.pinimg.com/originals/9c/5c/23/9c5c23a403abf0226276d4724396fa18.jpg', description: 'Hermosa ciudad'},
     // tslint:disable-next-line: max-line-length
-    {id: 10, name: 'Jericoacoara', img: 'http://www.wkndheroes.com/wp-content/uploads/2016/03/lagoa-paraiso-jeri-1200x640.jpg', description: 'Hermosa ciudad'},
+    {id: 7, name: 'Fortaleza', state: 'Ceará', img: 'https://www.turismobr.com/wp-content/uploads/2011/01/Fortaleza-desde-sus-playas-760x500.jpg', description: 'Hermosa ciudad'},
     // tslint:disable-next-line: max-line-length
-    {id: 11, name: 'Belo Horizonte', img: 'https://st2.depositphotos.com/2121749/11739/i/950/depositphotos_117395548-stock-photo-metropolis-of-belo-horizonte-minas.jpg', description: 'Hermosa ciudad'},
-    {id: 12, name: 'Búzios', img: 'https://images.clarin.com/2016/10/05/HJQeZ93YNl_1256x620.jpg', description: 'Hermosa ciudad'},
+    {id: 8, name: 'Natal', state: 'Río Grande do Norte', img: 'https://d1cfr5yzsdeyhu.cloudfront.net/blog/wp-content/uploads/2016/04/12162253/Tips-para-viajar-a-Natal.jpg', description: 'Hermosa ciudad'},
     // tslint:disable-next-line: max-line-length
-    {id: 13, name: 'Manaos', img: 'https://www.elterritorio.com.ar/verimg.aspx?F=1&A=594&O=/img/1/156/9547720644070972_1.jpg', description: 'Hermosa ciudad'},
+    {id: 9, name: 'Trancoso', state: 'Brasil', img: 'https://dla.cdncimeco.com/uploads/2018/12/image5c27e80068e06.jpg', description: 'Hermosa ciudad'},
     // tslint:disable-next-line: max-line-length
-    {id: 14, name: 'Foz do Iguaçu', img: 'http://www.loumarturismo.com.br/img/passeios-860x600/cataratas-860x600.jpg', description: 'Hermosa ciudad'},
-    {id: 15, name: 'Porto Seguro', img: 'https://images.clarin.com/2008/11/21/r1ES_ootNe_1200x0.jpg', description: 'Hermosa ciudad'},
+    {id: 10, name: 'Jericoacoara', state: 'Ceará', img: 'http://www.wkndheroes.com/wp-content/uploads/2016/03/lagoa-paraiso-jeri-1200x640.jpg', description: 'Hermosa ciudad'},
+    // tslint:disable-next-line: max-line-length
+    {id: 11, name: 'Belo Horizonte', state: 'Minas Gerais', img: 'https://st2.depositphotos.com/2121749/11739/i/950/depositphotos_117395548-stock-photo-metropolis-of-belo-horizonte-minas.jpg', description: 'Hermosa ciudad'},
+    // tslint:disable-next-line: max-line-length
+    {id: 12, name: 'Búzios', state: 'Rio De Janeiro', img: 'https://images.clarin.com/2016/10/05/HJQeZ93YNl_1256x620.jpg', description: 'Hermosa ciudad'},
+    // tslint:disable-next-line: max-line-length
+    {id: 13, name: 'Manaos', state: 'Amazonas', img: 'https://www.elterritorio.com.ar/verimg.aspx?F=1&A=594&O=/img/1/156/9547720644070972_1.jpg', description: 'Hermosa ciudad'},
+    // tslint:disable-next-line: max-line-length
+    {id: 14, name: 'Foz do Iguaçu', state: 'Paraná', img: 'http://www.loumarturismo.com.br/img/passeios-860x600/cataratas-860x600.jpg', description: 'Hermosa ciudad'},
+    // tslint:disable-next-line: max-line-length
+    {id: 15, name: 'Porto Seguro', state: 'Bahía', img: 'https://images.clarin.com/2008/11/21/r1ES_ootNe_1200x0.jpg', description: 'Hermosa ciudad'},
 ];
