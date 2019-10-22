9c54f29be51516355b9504c341796933616610e2
Gero <geronimo@devsar.com>
2019-10-22 15:00:08 -0300

    Gallery basic structure and data finished

--- src/app/components/inn/inn-gallery/inn-gallery.component.ts
+++ src/app/components/inn/inn-gallery/inn-gallery.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Inn } from 'src/app/types/inn';
 
 @Component({
   selector: 'app-inn-gallery',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inn-gallery.component.scss'],
 })
 export class InnGalleryComponent implements OnInit {
+
+  @Input() inndata: Inn;
 
   constructor() { }
 
