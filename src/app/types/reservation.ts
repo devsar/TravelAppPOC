97b0572c0ef318d822d360cec372ed4668d63e0a
Gero <geronimo@devsar.com>
2019-10-24 12:52:55 -0300

    Added reservation data type

---
+++ src/app/types/reservation.ts
@@ -0,0 +1,10 @@
+export class Reservation {
+    id: number;
+    dateIn: string;
+    dateOut: string;
+    pricePerNight: number;
+    platformCharge: number;
+    taxes: number;
+    adults: number;
+    children: number;
+}
