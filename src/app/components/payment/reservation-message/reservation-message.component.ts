8ad430406c43d67ee75d8a08067e6a21c8bfad59
Gero <geronimo@devsar.com>
2019-10-25 10:17:09 -0300

    Reservation message basic structure set

--- src/app/components/payment/reservation-message/reservation-message.component.ts
+++ src/app/components/payment/reservation-message/reservation-message.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Inn } from 'src/app/types/inn';
 
 @Component({
   selector: 'app-reservation-message',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reservation-message.component.scss'],
 })
 export class ReservationMessageComponent implements OnInit {
+
+  @Input() inndata: Inn;
 
   constructor() { }
 
