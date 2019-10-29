68a228367d63e03692f5d7a36f990d997a404002
Gero <geronimo@devsar.com>
2019-10-29 09:51:07 -0300

    Added cities description popup

--- src/app/components/home/tarjeta-ciudad/tarjeta-ciudad.component.ts
+++ src/app/components/home/tarjeta-ciudad/tarjeta-ciudad.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tarjeta-ciudad',
@@ -9,8 +10,20 @@ export class TarjetaCiudadComponent implements OnInit {
 
   @Input() citydata;
 
-  constructor() { }
+  constructor(public alertController: AlertController) { }
 
   ngOnInit() {}
+
+  async openDescription(event, city: string, state: string, description: string) {
+    event.stopPropagation();
+    const alert = await this.alertController.create({
+      header: city,
+      subHeader: 'Estado de ' + state,
+      message: description,
+      buttons: ['Genial!']
+    });
+
+    await alert.present();
+  }
 
 }
